8295d50ce135d0bb3f3d11dc70156d4643147608
yahia-y4 <yalhmwd994@gmail.com>
2026-04-07 20:07:30 +0300

    create infoWin component  - c18

---
+++ src/components/InfoWin/InfoWin.tsx
@@ -0,0 +1,18 @@
+
+import "./InfoWin.css"
+
+type Props = {
+    title?:string;
+    data?:string;
+    w?:string;
+    h?:string;
+}
+
+export default function InfoWin({title, data, w,h}: Props) {
+    return(
+        <div className="InfoWin-div" style={{width:w , height:h}}>
+            <h2 className="InfoWin-title">{title}</h2>
+            <p className="InfoWin-data">( {data} )</p>
+        </div>
+    )
+}
